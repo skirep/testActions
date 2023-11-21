809f0a68eb1263da58cc92996ab54e8bb1a65b9a
Oriol <skirep@gmail.com>
2023-11-21 14:28:54 +0100

    updated py file and removed chromedriver files

--- googleScreen.py
+++ googleScreen.py
@@ -1,29 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import time
 
-# Especifica la ubicación del chromedriver.exe en tu sistema
-# Puedes descargarlo desde https://sites.google.com/chromium.org/driver/
-chromedriver_path = './chromedriver'
-
-# Crea una instancia del navegador Chrome
+service = Service()
 options = webdriver.ChromeOptions()
-
-# Deshabilita la interfaz gráfica de usuario (UI) para ejecutar en segundo plano
 options.add_argument('--headless')
-
-# Inicia el navegador con las opciones configuradas
-browser = webdriver.Chrome(executable_path=chromedriver_path, options=options)
+driver = webdriver.Chrome(service=service, options=options)
 
 try:
-    # Abre la página de Google
-    browser.get('https://www.google.com')
+    # Open Google
+    driver.get('https://www.google.com')
 
-    # Espera a que la página se cargue completamente (puedes ajustar este tiempo según sea necesario)
-    browser.implicitly_wait(5)
+    # Wait 5 seconds
+    driver.implicitly_wait(5)
 
-    # Toma una captura de pantalla y guárdala en un archivo
-    browser.save_screenshot('captura_de_pantalla.png')
-    print("Captura de pantalla exitosa. Archivo guardado como 'captura_de_pantalla.png'.")
+    timestr = "capture-" + time.strftime("%Y%m%d-%H%M%S") + ".png"
+
+    # Make screenshot
+    driver.save_screenshot(timestr)
+    print("Screenshot done.")
+
 
 finally:
     # Cierra el navegador
-    browser.quit()
+    driver.quit()
