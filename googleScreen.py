c6b6a7a0f0d000663907fb883fc05d237f4c0d8f
Oriol <skirep@gmail.com>
2023-11-22 13:33:22 +0100

    get WEB_URL to make screeshot

--- googleScreen.py
+++ googleScreen.py
@@ -1,18 +1,20 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+import os
 import time
 
 service = Service()
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
+options.add_argument('--headless --window-size=1920,1080')
+
 driver = webdriver.Chrome(service=service, options=options)
 
 try:
     # Open Google
-    driver.get('https://www.google.com')
+    driver.get(os.environ["WEB_URL"])
 
     # Wait 5 seconds
-    driver.implicitly_wait(5)
+    driver.implicitly_wait(8)
 
     timestr = "capture-" + time.strftime("%Y%m%d-%H%M%S") + ".png"
 
